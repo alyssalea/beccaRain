c5eee35d81acf16573617c6eb24ad44be8f09977
alyssalea <alyssalea@icloud.com>
2019-05-20 22:57:09 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,272 @@
+var x = 400
+var i = 200
+var b = 0
+var z = -200
+var y = 0
+var s = 250
+var a = 25
+var q = 25
+var p = -800
+
+
+
+ 
+function setup() {
+  createCanvas(500, 800)
+  frameRate(20)
+  
+}
+
+function draw() {
+  background(75)
+ 
+ 
+
+  fill(147, 208, 255, 85)
+  noStroke()
+  
+  ellipse(random(1, 500), y, 8)
+  ellipse(random(1, 500), y-10, 8)
+  ellipse(random(1, 500), y - 20, 8)
+  ellipse(random(1, 500), y- 30, 8)
+  ellipse(random(1, 500), y - 40, 8)
+  ellipse(random(1, 500), y - 50, 8)
+  ellipse(random(1, 500), y - 60, 8)
+  ellipse(random(1, 500), y - 70, 8)
+  ellipse(random(1, 500), y - 80, 8)
+  ellipse(random(1, 500), y - 90, 8)
+  ellipse(random(1, 500), y - 100, 8)
+  ellipse(random(1, 500), y - 110, 8)
+  ellipse(random(1, 500), y - 120, 8)
+  ellipse(random(1, 500), y - 130, 8)
+  ellipse(random(1, 500), y - 140, 8)
+  ellipse(random(1, 500), y - 150, 8)
+  ellipse(random(1, 500), y - 160, 8)
+  ellipse(random(1, 500), y - 170, 8)
+  ellipse(random(1, 500), y - 180, 8)
+  ellipse(random(1, 500), y - 190, 8)
+  ellipse(random(1, 500), y - 200, 8)
+  ellipse(random(1, 500), y - 210, 8)
+  ellipse(random(1, 500), y - 220, 8)
+  ellipse(random(1, 500), y - 230, 8)
+  ellipse(random(1, 500), y - 240, 8)
+  ellipse(random(1, 500), y - 250, 8)
+  ellipse(random(1, 500), y - 260, 8)
+  ellipse(random(1, 500), y - 270, 8)
+  ellipse(random(1, 500), y - 280, 8)
+  ellipse(random(1, 500), y - 290, 8)
+  ellipse(random(1, 500), y - 300, 8)
+  ellipse(random(1, 500), y - 310, 8)
+  ellipse(random(1, 500), y - 320, 8)
+  ellipse(random(1, 500), y - 330, 8)
+  ellipse(random(1, 500), y - 340, 8)
+  ellipse(random(1, 500), y - 350, 8)
+  ellipse(random(1, 500), y - 360, 8)
+  ellipse(random(1, 500), y - 370, 8)
+  ellipse(random(1, 500), y - 380, 8)
+  ellipse(random(1, 500), y - 390, 8)
+  ellipse(random(1, 500), y - 400, 8)
+  ellipse(random(1, 500), y - 410, 8)
+  ellipse(random(1, 500), y - 420, 8)
+  ellipse(random(1, 500), y - 430, 8)
+  ellipse(random(1, 500), y - 440, 8)
+  ellipse(random(1, 500), y - 450, 8)
+  ellipse(random(1, 500), y - 460, 8)
+  ellipse(random(1, 500), y - 470, 8)
+  ellipse(random(1, 500), y - 480, 8)
+  ellipse(random(1, 500), y - 490, 8)
+  ellipse(random(1, 500), y - 500, 8)
+  ellipse(random(1, 500), y - 510, 8)
+  ellipse(random(1, 500), y - 520, 8)
+  ellipse(random(1, 500), y - 530, 8)
+  ellipse(random(1, 500), y - 540, 8)
+  ellipse(random(1, 500), y - 550, 8)
+  ellipse(random(1, 500), y - 560, 8)
+  ellipse(random(1, 500), y - 570, 8)
+  ellipse(random(1, 500), y - 580, 8)
+  ellipse(random(1, 500), y - 590, 8)
+  ellipse(random(1, 500), y - 600, 8)
+  ellipse(random(1, 500), y- 610, 8)
+  ellipse(random(1, 500), y - 620, 8)
+  ellipse(random(1, 500), y- 630, 8)
+  ellipse(random(1, 500), y - 640, 8)
+  ellipse(random(1, 500), y - 650, 8)
+  ellipse(random(1, 500), y - 660, 8)
+  ellipse(random(1, 500), y - 670, 8)
+  ellipse(random(1, 500), y - 680, 8)
+  ellipse(random(1, 500), y - 690, 8)
+  ellipse(random(1, 500), y - 700, 8)
+  ellipse(random(1, 500), y - 710, 8)
+  ellipse(random(1, 500), y - 720, 8)
+  ellipse(random(1, 500), y - 730, 8)
+  ellipse(random(1, 500), y - 740, 8)
+  ellipse(random(1, 500), y - 750, 8)
+  ellipse(random(1, 500), y - 760, 8)
+  ellipse(random(1, 500), y - 770, 8)
+  ellipse(random(1, 500), y - 780, 8)
+  ellipse(random(1, 500), y - 790, 8)
+  ellipse(random(1, 500), y - 800, 8)
+  
+  y += 2
+  
+  
+  if (y > 1600){
+  y = 0
+  }
+  
+  
+  ellipse(random(1, 500), p, 8)
+  ellipse(random(1, 500), p-10, 8)
+  ellipse(random(1, 500), p - 20, 8)
+  ellipse(random(1, 500), p- 30, 8)
+  ellipse(random(1, 500), p - 40, 8)
+  ellipse(random(1, 500), p - 50, 8)
+  ellipse(random(1, 500), p - 60, 8)
+  ellipse(random(1, 500), p - 70, 8)
+  ellipse(random(1, 500), p - 80, 8)
+  ellipse(random(1, 500), p - 90, 8)
+  ellipse(random(1, 500), p - 100, 8)
+  ellipse(random(1, 500), p - 110, 8)
+  ellipse(random(1, 500), p - 120, 8)
+  ellipse(random(1, 500), p - 130, 8)
+  ellipse(random(1, 500), p - 140, 8)
+  ellipse(random(1, 500), p - 150, 8)
+  ellipse(random(1, 500), p - 160, 8)
+  ellipse(random(1, 500), p - 170, 8)
+  ellipse(random(1, 500), p - 180, 8)
+  ellipse(random(1, 500), p - 190, 8)
+  ellipse(random(1, 500), p - 200, 8)
+  ellipse(random(1, 500), p - 210, 8)
+  ellipse(random(1, 500), p - 220, 8)
+  ellipse(random(1, 500), p - 230, 8)
+  ellipse(random(1, 500), p - 240, 8)
+  ellipse(random(1, 500), p - 250, 8)
+  ellipse(random(1, 500), p - 260, 8)
+  ellipse(random(1, 500), p - 270, 8)
+  ellipse(random(1, 500), p - 280, 8)
+  ellipse(random(1, 500), p - 290, 8)
+  ellipse(random(1, 500), p - 300, 8)
+  ellipse(random(1, 500), p - 310, 8)
+  ellipse(random(1, 500), p - 320, 8)
+  ellipse(random(1, 500), p - 330, 8)
+  ellipse(random(1, 500), p - 340, 8)
+  ellipse(random(1, 500), p - 350, 8)
+  ellipse(random(1, 500), p - 360, 8)
+  ellipse(random(1, 500), p - 370, 8)
+  ellipse(random(1, 500), p - 380, 8)
+  ellipse(random(1, 500), p- 390, 8)
+  ellipse(random(1, 500), p - 400, 8)
+  ellipse(random(1, 500), p - 410, 8)
+  ellipse(random(1, 500), p - 420, 8)
+  ellipse(random(1, 500), p - 430, 8)
+  ellipse(random(1, 500), p- 440, 8)
+  ellipse(random(1, 500), p - 450, 8)
+  ellipse(random(1, 500), p - 460, 8)
+  ellipse(random(1, 500), p - 470, 8)
+  ellipse(random(1, 500), p - 480, 8)
+  ellipse(random(1, 500), p - 490, 8)
+  ellipse(random(1, 500), p - 500, 8)
+  ellipse(random(1, 500), p- 510, 8)
+  ellipse(random(1, 500), p - 520, 8)
+  ellipse(random(1, 500), p - 530, 8)
+  ellipse(random(1, 500), p - 540, 8)
+  ellipse(random(1, 500), p - 550, 8)
+  ellipse(random(1, 500), p - 560, 8)
+  ellipse(random(1, 500), p - 570, 8)
+  ellipse(random(1, 500), p - 580, 8)
+  ellipse(random(1, 500), p - 590, 8)
+  ellipse(random(1, 500), p - 600, 8)
+  ellipse(random(1, 500), p- 610, 8)
+  ellipse(random(1, 500), p - 620, 8)
+  ellipse(random(1, 500), p- 630, 8)
+  ellipse(random(1, 500), p - 640, 8)
+  ellipse(random(1, 500), p - 650, 8)
+  ellipse(random(1, 500), p - 660, 8)
+  ellipse(random(1, 500), p - 670, 8)
+  ellipse(random(1, 500), p - 680, 8)
+  ellipse(random(1, 500), p - 690, 8)
+  ellipse(random(1, 500), p - 700, 8)
+  ellipse(random(1, 500), p - 710, 8)
+  ellipse(random(1, 500), p- 720, 8)
+  ellipse(random(1, 500), p - 730, 8)
+  ellipse(random(1, 500), p - 740, 8)
+  ellipse(random(1, 500), p - 750, 8)
+  ellipse(random(1, 500), p - 760, 8)
+  ellipse(random(1, 500), p - 770, 8)
+  ellipse(random(1, 500), p - 780, 8)
+  ellipse(random(1, 500), p - 790, 8)
+  ellipse(random(1, 500), p - 800, 8)
+  
+  p += 2
+  
+  
+  if (p > 1600){
+  p = -800
+  }
+  
+  
+  
+  
+  //coud 1
+  fill(180)
+  noStroke(0)
+  ellipse(x, 75-q, 190, 75)
+  ellipse(x + 50, 50-q, 65, 50)
+  ellipse(x, 40-q, 65, 50)
+  ellipse(x - 50, 50-q, 65, 50)
+  ellipse(x - 50, 100-q, 65, 50)
+  ellipse(x, 110-q, 65, 50)
+  ellipse(x + 50, 100-q, 65, 50)
+  
+  x = x + .5 
+  
+  if (x > 600){
+  x = -100
+  }
+  
+  //cloud 2 
+  ellipse(i, 75 - a, 190, 75)
+  ellipse(i + 50, 50 - a, 65, 50)
+  ellipse(i, 40 - a, 65, 50)
+  ellipse(i - 50, 50 - a, 65, 50)
+  ellipse(i - 50, 100- a, 65, 50)
+  ellipse(i, 110 - a, 65, 50)
+  ellipse(i + 50, 100 - a, 65, 50)
+  
+  i += .5
+  
+  if (i > 600){
+  i = -100
+  }
+  
+  //cloud 3 
+  ellipse(b, 75- a, 190, 75)
+  ellipse(b + 50, 50- a, 65, 50)
+  ellipse(b, 40- a, 65, 50)
+  ellipse(b - 50, 50 - a, 65, 50)
+  ellipse(b - 50, 100 - a, 65, 50)
+  ellipse(b, 110- a, 65, 50)
+  ellipse(b + 50, 100- a, 65, 50)
+  
+  b = b + .5
+  
+  if (b > 600){
+  b = -100
+  }
+  
+  ellipse(z, 75- a, 190, 75)
+  ellipse(z + 50, 50- a, 65, 50)
+  ellipse(z, 40- a, 65, 50)
+  ellipse(z - 50, 50- a, 65, 50)
+  ellipse(z - 50, 100- a, 65, 50)
+  ellipse(z, 110- a, 65, 50)
+  ellipse(z + 50, 100- a, 65, 50)
+  
+  if (z > 600){
+  z = -100
+  }
+  
+  
+  
+  
+}
